2f47d017d23857c31732e20b7dafcb27add3e1c0
Mejbah Uddin Shameem <mejbah_uddin.shameem@dfki.de>
2025-02-08 09:45:00 +0100

    Rewrite service layer to match backend API contracts

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -3,7 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
-import { User } from '../models/user.model';
+import { User, MessageResponse } from '../models/user.model';
+import { CreatedWorkflow, FollowedWorkflow, VotingHistoryItem } from '../models/workflow.model';
+import { TaskInstance } from '../models/task.model';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
@@ -11,17 +13,44 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
-  getProfile(): Observable<User> {
-    return this.http.get<User>(`${this.apiUrl}/users/me`);
+  updateProfile(data: { name?: string; password?: string; confirmPassword?: string }): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/users/me`, data);
   }
 
-  updateProfile(data: Partial<User>): Observable<User> {
-    return this.http.patch<User>(`${this.apiUrl}/users/me`, data);
-  }
-
-  uploadAvatar(file: File): Observable<any> {
+  uploadAvatar(file: File): Observable<string> {
     const formData = new FormData();
     formData.append('avatar', file);
-    return this.http.post(`${this.apiUrl}/users/me/avatar`, formData);
+    return this.http.post(`${this.apiUrl}/users/me/avatar`, formData, { responseType: 'text' });
+  }
+
+  getCreatedWorkflows(): Observable<CreatedWorkflow[]> {
+    return this.http.get<CreatedWorkflow[]>(`${this.apiUrl}/user/me/created-workflows/all`);
+  }
+
+  getFollowedWorkflows(): Observable<FollowedWorkflow[]> {
+    return this.http.get<FollowedWorkflow[]>(`${this.apiUrl}/users/me/workflowinstance/following/all`);
+  }
+
+  getFollowedWorkflowTasks(instanceId: string): Observable<TaskInstance[]> {
+    return this.http.get<TaskInstance[]>(`${this.apiUrl}/following/workflow/${instanceId}/tasks/all`);
+  }
+
+  startTask(instanceId: string, taskId: string): Observable<TaskInstance> {
+    return this.http.post<TaskInstance>(`${this.apiUrl}/following/workflow/${instanceId}/task/${taskId}/start`, {});
+  }
+
+  endTask(instanceId: string, taskId: string): Observable<MessageResponse> {
+    return this.http.post<MessageResponse>(`${this.apiUrl}/following/workflow/${instanceId}/task/${taskId}/end`, {});
+  }
+
+  toggleNotification(instanceId: string, taskId: string, enabled: boolean): Observable<MessageResponse> {
+    return this.http.post<MessageResponse>(
+      `${this.apiUrl}/following/workflow/${instanceId}/task/${taskId}/notify`,
+      { task_notification: enabled }
+    );
+  }
+
+  getVotingHistory(): Observable<VotingHistoryItem[]> {
+    return this.http.get<VotingHistoryItem[]>(`${this.apiUrl}/user/voting/history`);
   }
 }

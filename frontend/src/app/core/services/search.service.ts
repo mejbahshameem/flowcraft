c25d7da051d03ac7b6b28e4b184acabcbdd937c3
Mejbah Uddin Shameem <mejbah_uddin.shameem@dfki.de>
2025-02-08 09:45:00 +0100

    Rewrite service layer to match backend API contracts

--- frontend/src/app/core/services/search.service.ts
+++ frontend/src/app/core/services/search.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
 import { Workflow } from '../models/workflow.model';
@@ -12,10 +12,22 @@ export class SearchService {
   constructor(private http: HttpClient) {}
 
   search(query: string, sortBy?: string): Observable<Workflow[]> {
-    let params = new HttpParams().set('query', query);
-    if (sortBy) {
-      params = params.set('sortBy', sortBy);
+    let params = new HttpParams().set('interest', query);
+    if (sortBy === 'popular') {
+      params = params.set('sortBy', 'up_vote');
     }
-    return this.http.get<Workflow[]>(`${this.apiUrl}/search`, { params });
+    return this.http.get<any[]>(`${this.apiUrl}/search`, { params }).pipe(
+      map(results => results.map(r => ({
+        _id: r._id,
+        name: r.name,
+        description: r.description,
+        location: r.location,
+        access: r.access,
+        owner: r.owner,
+        upvotes: r.voting?.up_vote?.length || 0,
+        downvotes: r.voting?.down_vote?.length || 0,
+        isDeleted: r.deleted || false,
+      })))
+    );
   }
 }

b07df1889beb6838700e4ef986d74768816c2674
Mejbah Uddin Shameem <mejbah_uddin.shameem@dfki.de>
2026-04-21 23:03:55 +0200

    Return empty list when user follows no workflows

--- Backend/src/routers/userworkflowcontrol.js
+++ Backend/src/routers/userworkflowcontrol.js
@@ -28,9 +28,9 @@ router.get(
 		try {
 			const user = await User.findById(req.user._id);
 
-			//check if number of workflow following is zero
+			//return empty list for users not following any workflow yet
 			if (user.followedworkflow.length === 0) {
-				return res.status(404).send();
+				return res.status(200).send([]);
 			}
 
 			// checking unfinished workflow
